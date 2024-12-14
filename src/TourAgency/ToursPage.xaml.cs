6c9138bf1092dd85bab5da9825103674b09c6900
inactive33 <325vi7001@gmail.com>
2024-12-14 04:45:00 +0300

    восстановление корректной работы #1

---
+++ src/TourAgency/ToursPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TourAgency.Entities;
+
+namespace TourAgency
+{
+    /// <summary>
+    /// Логика взаимодействия для ToursPage.xaml
+    /// </summary>
+    public partial class ToursPage : Page
+    {
+        private Tour _currentTour = new Tour();
+        public ToursPage()
+        {
+            InitializeComponent();
+            DGridTour.ItemsSource = TourAgencyEntities.GetContext().Tours.ToList();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            //Tour.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Tour));
+        }
+    }
+}
