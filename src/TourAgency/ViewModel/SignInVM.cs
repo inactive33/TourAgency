3b1812f638e7b5dc98e705bde6657479067cd175
inactive33 <325vi7001@gmail.com>
2024-12-18 08:14:16 +0300

    useless usings delete

--- src/TourAgency/ViewModel/SignInVM.cs
+++ src/TourAgency/ViewModel/SignInVM.cs
@@ -1,11 +1,6 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using System;
-using System.Configuration;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TourAgency.Entities;
 using TourAgency.Models;
 
